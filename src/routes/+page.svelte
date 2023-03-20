c4b8d21db144d9c9c495faa1377a2576117ed497
RikkertTheDeveloper <rick.arendsen@student.graafschapcollege.nl>
2023-03-20 15:36:25 +0100

    [1.0.2] - Add Build Pipeline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-bold">RickArendsen.nl</h1>
+<p>SvelteKit is aan het functioneren.</p>
