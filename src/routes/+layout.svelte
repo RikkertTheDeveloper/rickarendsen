062c37495aa896fc74a12ad610a41bf6ab9e15e7
RikkertTheDeveloper <rick.arendsen@student.graafschapcollege.nl>
2023-03-20 16:29:25 +0100

    [1.0.4] - Add Components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
-  import "../app.css";
+    import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+    import '@skeletonlabs/skeleton/styles/all.css';
+    import "../app.css";
+
+    import {AppBar, LightSwitch} from "@skeletonlabs/skeleton"
+    import Home from "svelte-material-icons/Home.svelte";
+    import Github from "svelte-material-icons/Github.svelte";
 </script>
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead"><Home size=25/></svelte:fragment>
+
+	<svelte:fragment slot="trail">
+        <LightSwitch />
+        <Github size=25 />
+    </svelte:fragment>
+</AppBar>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1 class="font-bold">RickArendsen.nl</h1>
-<p>SvelteKit is aan het functioneren.</p>
+<script>
+    import Github from "svelte-material-icons/Github.svelte";
+</script>
+
+<div class="container m-auto text-center mt-5">
+    <h1 class="font-black">Rick Arendsen</h1>
+    <span class="text-2xl">Full Stack Developer</span>
+
+    <div class="badge-list flex justify-center mt-2 flex-wrap space-x-2">
+        <span class="badge variant-filled">SvelteKit</span>
+        <span class="badge variant-soft">Laravel</span>
+        <span class="badge variant-soft">Nuxt</span>
+        <span class="badge variant-soft">Vue</span>
+    </div>
+</div>
